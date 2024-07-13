bf38f11bd38cd6331d6c023aef9f901e72fb241d
Jordi Ortolá Ankum <Tomasito665@users.noreply.github.com>
2024-07-13 02:00:19 +0200

    3: Initialize Gatsby project with simple 'Hello world' homepage (#1)

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Micro-Commute`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import * as React from "react"
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+const headingAccentStyles = {
+  color: "#663399",
+}
+const paragraphStyles = {
+  marginBottom: 48,
+}
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>
+        Micro-Commute
+        <br />
+        <span style={headingAccentStyles}>— shared mobility for your commute 🌱🚴🏡</span>
+      </h1>
+      <p style={paragraphStyles}>
+        Solving the first and last mile problem for urban commuters.
+      </p>
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Micro-Commute</title>
