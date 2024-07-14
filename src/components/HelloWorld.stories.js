907c4a27a7fbbe0b8b8ac8259e1d004630529d35
Jordi Ortolá Ankum <Tomasito665@users.noreply.github.com>
2024-07-15 01:39:23 +0200

    MC-15: Create 'hello world' component (#7)
    Co-authored-by: Victor Ernest <vernest37@yahoo.co.uk>

---
+++ src/components/HelloWorld.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import HelloWorld from './HelloWorld';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Components/HelloWorld',
+  component: HelloWorld,
+  parameters: {
+    layout: 'centered', // Optional parameter to center the component in the Canvas
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default = {
+  // Use the spread operator (...) to include any default props from your HelloWorld component
+  args: { ...HelloWorld.defaultProps },
+};
