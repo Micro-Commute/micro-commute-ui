55cb59b17dbe09c36dc7b5ed52549906f220b070
Jordi Ortolá Ankum <Tomasito665@users.noreply.github.com>
2024-07-31 19:27:07 +0200

    MC-24: Add empty route map component (#10)

---
+++ src/components/RouteMap/RouteMap.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {MapContainer, TileLayer} from "react-leaflet";
+import "leaflet/dist/leaflet.css"
+import {isDomAvailable} from "../../lib/util";
+
+export default function RouteMap() {
+  if (!isDomAvailable()) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <MapContainer style={{ height: "400px" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  )
+}
