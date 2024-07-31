f0e781b219fa805cae4ca3c69d865570afd29e01
Jordi <jordi665@hotmail.com>
2024-07-31 11:47:56 +0200

    MC-24: Add route map reactjs component and story

---
+++ src/components/RouteMap/RouteMap.stories.js
@@ -0,0 +1,12 @@
+import RouteMap from "./RouteMap";
+
+export default {
+  title: "Components/RouteMap",
+  component: RouteMap,
+  tags: ["autodocs"],
+}
+
+export const Default = {
+  args: {
+  }
+}
