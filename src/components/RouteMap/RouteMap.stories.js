9c274c254ad0ff4ee5ad4ca8325eacd45f37655d
Jordi Ortolá Ankum <Tomasito665@users.noreply.github.com>
2024-08-03 12:09:26 +0200

    MC-24: Render docked ebike routes on map 'as the crow flies' (#13)

--- src/components/RouteMap/RouteMap.stories.js
+++ src/components/RouteMap/RouteMap.stories.js
@@ -10,3 +10,15 @@ export const Default = {
   args: {
   }
 }
+
+export const DockedEbike = {
+  args: {
+    route: {
+      type: "docked-ebike",
+      startingPoint: [41.40624877362421, 2.159413247558875],
+      startingPointDockingStation: [41.40568486276983, 2.1624520213408296],
+      destinationDockingStation: [41.38511382647192, 2.1431890499002897],
+      destination: [41.38413627190235, 2.145163260838563],
+    }
+  }
+}
