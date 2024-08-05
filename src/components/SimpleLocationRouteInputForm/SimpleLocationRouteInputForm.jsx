7d59ed622b6249ade062f5f22edd7a2385f4e2b3
Victor Ernest <vernest37@yahoo.co.uk>
2024-08-05 13:33:43 +0100

    MC-22: Modify Simple Location Route Input Form

--- src/components/SimpleLocationRouteInputForm/SimpleLocationRouteInputForm.jsx
+++ src/components/SimpleLocationRouteInputForm/SimpleLocationRouteInputForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import LocationInput from './LocationInput';
+import LocationInput from '../LocationInput';
 
 const SimpleLocationRouteInputForm = ({ onStartingPointChange, onDestinationChange }) => {
   return (
