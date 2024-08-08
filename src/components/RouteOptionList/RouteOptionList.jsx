ebd068ad0d48bbcc3cbd8f63990d0f0bb081cf54
Jordi <jordi665@hotmail.com>
2024-08-08 19:25:24 +0200

    MC-25: Remove unknown prop and unnecessary check from RouteOptionList

--- src/components/RouteOptionList/RouteOptionList.jsx
+++ src/components/RouteOptionList/RouteOptionList.jsx
@@ -1,41 +1,19 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import RouteOption from "../RouteOption/RouteOption";
-
-// export default function RouteOptionList({routeOptionProps}) {
-//   return (
-//     <div>
-//       {routeOptionProps.map(props => RouteOption(props))}
-//     </div>
-//   );
-// }
-
-// RouteOptionList.propTypes = {
-//   routeOptionProps: PropTypes.arrayOf(RouteOption.propTypes),
-// }
-
-
-
-
-import React, { useState } from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import RouteOption from "../RouteOption/RouteOption";
 
-export default function RouteOptionList({ routeOptionProps, onRouteOptionSelected }) {
+export default function RouteOptionList({routeOptionProps, onRouteOptionSelected}) {
   const [selectedIndex, setSelectedIndex] = useState(null);
 
   const handleOptionClick = (index, providerId, routeType) => {
     setSelectedIndex(index);
-    if (onRouteOptionSelected) {
-      onRouteOptionSelected(providerId, routeType);
-    }
+    onRouteOptionSelected(providerId, routeType);
   };
 
   return (
     <div>
       {routeOptionProps.map((props, index) => (
         <RouteOption
-          key={index}
           {...props}
           isSelected={index === selectedIndex}
           onClick={() => handleOptionClick(index, props.provider.name, props.type)}
@@ -48,4 +26,4 @@ export default function RouteOptionList({ routeOptionProps, onRouteOptionSelecte
 RouteOptionList.propTypes = {
   routeOptionProps: PropTypes.arrayOf(PropTypes.shape(RouteOption.propTypes)),
   onRouteOptionSelected: PropTypes.func.isRequired,
-};
+}
