c179cbb29a022cb193b0cfd2c4988a38b1163863
Jordi <jordi665@hotmail.com>
2024-08-09 01:37:49 +0200

    MC-25: Pre-select first route option in list

--- src/components/RouteOptionList/RouteOptionList.jsx
+++ src/components/RouteOptionList/RouteOptionList.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import RouteOption from "../RouteOption/RouteOption";
 
 export default function RouteOptionList({routeOptionProps, onRouteOptionSelected}) {
-  const [selectedIndex, setSelectedIndex] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleOptionClick = (index, provider, routeType) => {
     setSelectedIndex(index);
