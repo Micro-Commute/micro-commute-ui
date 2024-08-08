44e87439bbad7585fa79d415848abc944837f0b4
Jordi <jordi665@hotmail.com>
2024-08-08 19:12:30 +0200

    MC-25: Recover DockedEbike and DockedEbikeEmpty route option stories

--- src/components/RouteOption/RouteOption.stories.js
+++ src/components/RouteOption/RouteOption.stories.js
@@ -1,38 +1,3 @@
-// import RouteOption from "./RouteOption";
-// import { fn } from "@storybook/test";
-
-// export default {
-//   title: "Components/RouteOption",
-//   component: RouteOption,
-//   tags: ["autodocs"],
-// };
-
-// export const DockedEbike = {
-//   args: {
-//     type: "docked-ebike",
-//     provider: { name: "Some provider" },
-//     fromDockingStations: [
-//       { value: "foo", label: "Foo street, 123" },
-//       { value: "bar", label: "Bar street, 123" },
-//       { value: "baz", label: "Baz street, 123" },
-//     ],
-//     toDockingStations: [{ value: "qux", label: "Qux street, 123" }],
-//     onDockingStationChange: fn(),
-//   },
-// };
-
-// export const DockedEbikeEmpty = {
-//   args: {
-//     type: "docked-ebike",
-//     provider: { name: "Some provider" },
-//     fromDockingStations: [],
-//     toDockingStations: [],
-//     onDockingStationChange: fn(),
-//   },
-// };
-
-
-import React from "react";
 import RouteOption from "./RouteOption";
 import { fn } from "@storybook/test";
 
@@ -42,14 +7,42 @@ export default {
   tags: ["autodocs"],
 };
 
-export const Selected = {
+export const DockedEbike = {
   args: {
     type: "docked-ebike",
-    provider: { name: "Selected provider" },
+    provider: { name: "Some provider" },
     fromDockingStations: [
-      { value: "selectedFoo", label: "Selected Foo street, 123" },
+      { value: "foo", label: "Foo street, 123" },
+      { value: "bar", label: "Bar street, 123" },
+      { value: "baz", label: "Baz street, 123" },
     ],
-    toDockingStations: [{ value: "selectedQux", label: "Selected Qux street, 123" }],
+    toDockingStations: [{ value: "qux", label: "Qux street, 123" }],
+    onDockingStationChange: fn(),
+    isSelected: false,
+  },
+};
+
+export const DockedEbikeEmpty = {
+  args: {
+    type: "docked-ebike",
+    provider: { name: "Some provider" },
+    fromDockingStations: [],
+    toDockingStations: [],
+    onDockingStationChange: fn(),
+    isSelected: false,
+  },
+};
+
+export const DockedEbikeSelected = {
+  args: {
+    type: "docked-ebike",
+    provider: { name: "Some provider" },
+    fromDockingStations: [
+      { value: "foo", label: "Foo street, 123" },
+      { value: "bar", label: "Bar street, 123" },
+      { value: "baz", label: "Baz street, 123" },
+    ],
+    toDockingStations: [{ value: "qux", label: "Qux street, 123" }],
     onDockingStationChange: fn(),
     isSelected: true,
   },
