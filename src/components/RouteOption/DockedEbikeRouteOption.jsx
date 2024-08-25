cfb89ce5de594cb74848d2b99cbb5f4512da55dc
Victor Ernest <vernest37@yahoo.co.uk>
2024-08-25 21:35:33 +0100

    MC-38: Render travel times in the DockedEBikeRouteOption

--- src/components/RouteOption/DockedEbikeRouteOption.jsx
+++ src/components/RouteOption/DockedEbikeRouteOption.jsx
@@ -1,117 +1,3 @@
-// import React, { useId } from "react";
-// import PropTypes from "prop-types";
-// import Style from "react-style-proptype";
-// import { RouteOptionPropType } from "../../modules/types";
-
-// export default function DockedEbikeRouteOption({
-//   routeOption,
-//   isSelected,
-//   onClick,
-//   onStartingPointStationChange,
-//   onDestinationStationChange,
-// }) {
-//   const fromDockingStations = routeOption.nearByStations.startingPoint;
-//   const toDockingStations = routeOption.nearByStations.destination;
-//   const fromDockingStationId = routeOption.selectedStationIds.startingPoint;
-//   const toDockingStationId = routeOption.selectedStationIds.destination;
-
-//   function handleArticleClick(event) {
-//     if (event.target instanceof HTMLInputElement) {
-//       // Do not send click when clicking on an input element
-//       return;
-//     }
-//     onClick();
-//   }
-
-//   return (
-//     <article
-//       onClick={handleArticleClick}
-//       style={{ backgroundColor: isSelected ? "cyan" : "inherit" }}
-//       role="option"
-//       aria-selected={isSelected}
-//       aria-label={`Docked e-bike route option with ${routeOption.provider.name}.`}
-//     >
-//       <header>
-//         <h1>{routeOption.provider.name}</h1>
-//       </header>
-//       {fromDockingStations.length > 0 && toDockingStations.length > 0 ? (
-//         <div style={{ display: "flex" }}>
-//           <DockingStationSelector
-//             label="From docking station"
-//             value={fromDockingStationId}
-//             stations={fromDockingStations}
-//             onChange={onStartingPointStationChange}
-//             style={{ flex: 1 }}
-//           />
-//           <DockingStationSelector
-//             label="To docking station"
-//             value={toDockingStationId}
-//             stations={toDockingStations}
-//             onChange={onDestinationStationChange}
-//             style={{ flex: 1 }}
-//           />
-//         </div>
-//       ) : (
-//         <em>No nearby docking stations.</em>
-//       )}
-//     </article>
-//   );
-// }
-
-// DockedEbikeRouteOption.propTypes = {
-//   routeOption: RouteOptionPropType,
-//   isSelected: PropTypes.bool.isRequired,
-//   onClick: PropTypes.func.isRequired, // () => void
-//   onStartingPointStationChange: PropTypes.func.isRequired, // (stationId) => void
-//   onDestinationStationChange: PropTypes.func.isRequired, // (stationId) => void
-// };
-
-// /** Pre-condition: options.length > 0 */
-// function DockingStationSelector({ label, value, stations, onChange, style }) {
-//   const labelId = useId();
-
-//   const handleChange = (e) => {
-//     let stationId = e.target.value;
-//     onChange(stationId);
-//   };
-
-//   return (
-//     <div style={style}>
-//       <label style={{ display: "block" }} id={labelId}>
-//         {label}
-//       </label>
-//       <select
-//         value={value}
-//         onChange={handleChange}
-//         // Prevent DockedEbikeRouteOption 'onClick' when clicking on this select
-//         onClick={(e) => e.stopPropagation()}
-//         aria-labelledby={labelId}
-//       >
-//         {stations.map((station) => (
-//           <option value={station.id} key={station.id}>
-//             {station.name}
-//           </option>
-//         ))}
-//       </select>
-//     </div>
-//   );
-// }
-
-// DockingStationSelector.propTypes = {
-//   label: PropTypes.string.isRequired,
-//   value: PropTypes.string.isRequired,
-//   stations: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//     }),
-//   ),
-//   onChange: PropTypes.func.isRequired, // (dockingStationId) => void
-//   style: Style,
-// };
-
-
-
 import React, { useId } from "react";
 import PropTypes from "prop-types";
 import Style from "react-style-proptype";
@@ -124,25 +10,24 @@ export default function DockedEbikeRouteOption({
   onStartingPointStationChange,
   onDestinationStationChange,
 }) {
-  const fromDockingStations = routeOption.nearByStations.startingPoint;
-  const toDockingStations = routeOption.nearByStations.destination;
-  const fromDockingStationId = routeOption.selectedStationIds.startingPoint;
-  const toDockingStationId = routeOption.selectedStationIds.destination;
-
   const {
     departureTime,
     arrivalTime,
+    totalTravelTime,
     takeBikeTime,
     parkBikeTime,
-    totalTravelTime,
     walkingToDockingStation,
     bikingTime,
     walkingToDestination,
   } = routeOption.travelTimes;
 
+  const fromDockingStations = routeOption.nearByStations.startingPoint;
+  const toDockingStations = routeOption.nearByStations.destination;
+  const fromDockingStationId = routeOption.selectedStationIds.startingPoint;
+  const toDockingStationId = routeOption.selectedStationIds.destination;
+
   function handleArticleClick(event) {
     if (event.target instanceof HTMLInputElement) {
-      // Do not send click when clicking on an input element
       return;
     }
     onClick();
@@ -159,8 +44,9 @@ export default function DockedEbikeRouteOption({
       <header>
         <h1>{routeOption.provider.name}</h1>
       </header>
+
       {fromDockingStations.length > 0 && toDockingStations.length > 0 ? (
-        <div style={{ display: "flex", flexDirection: "column" }}>
+        <div>
           <div style={{ display: "flex" }}>
             <DockingStationSelector
               label="From docking station"
@@ -177,18 +63,68 @@ export default function DockedEbikeRouteOption({
               style={{ flex: 1 }}
             />
           </div>
-          <div style={{ marginTop: "10px" }}>
-            <h2>Travel Times:</h2>
-            <ul>
-              <li>Departure Time: {departureTime}</li>
-              <li>Arrival Time: {arrivalTime}</li>
-              <li>Take Bike Time: {takeBikeTime}</li>
-              <li>Park Bike Time: {parkBikeTime}</li>
-              <li>Total Travel Time: {totalTravelTime}</li>
-              <li>Walking to Docking Station: {walkingToDockingStation}</li>
-              <li>Biking Time: {bikingTime}</li>
-              <li>Walking to Destination: {walkingToDestination}</li>
-            </ul>
+          <div
+            style={{
+              marginTop: "16px",
+              borderTop: "1px solid #ccc",
+              paddingTop: "16px",
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              gridGap: "16px",
+            }}
+          >
+            {/* Column 1 */}
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div>
+                <div>Departure and arrival times</div>
+                <div>{`${departureTime} – ${arrivalTime}`}</div>
+              </div>
+              <div
+                style={{
+                  marginTop: "8px",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <div style={{ textAlign: "center" }}>
+                    <div>🧍</div>
+                    <div>{walkingToDockingStation}</div>
+                  </div>
+                  <div style={{ margin: "0 8px" }}>{">"}</div>
+                  <div style={{ textAlign: "center" }}>
+                    <div>🚴</div>
+                    <div>{bikingTime}</div>
+                  </div>
+                  <div style={{ margin: "0 8px" }}>{">"}</div>
+                  <div style={{ textAlign: "center" }}>
+                    <div>🧍</div>
+                    <div>{walkingToDestination}</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Column 2 */}
+            <div>
+              <div>
+                <div>Total travel time</div>
+                <div>{totalTravelTime}</div>
+              </div>
+              <div style={{ marginTop: "8px" }}></div>
+            </div>
+
+            {/* Column 3 */}
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div>
+                <div>Take bike at</div>
+                <div>{takeBikeTime}</div>
+              </div>
+              <div style={{ marginTop: "8px" }}>
+                <div>Park bike at</div>
+                <div>{parkBikeTime}</div>
+              </div>
+            </div>
           </div>
         </div>
       ) : (
@@ -201,31 +137,19 @@ export default function DockedEbikeRouteOption({
 DockedEbikeRouteOption.propTypes = {
   routeOption: RouteOptionPropType,
   isSelected: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired, // () => void
-  onStartingPointStationChange: PropTypes.func.isRequired, // (stationId) => void
-  onDestinationStationChange: PropTypes.func.isRequired, // (stationId) => void
+  onClick: PropTypes.func.isRequired,
+  onStartingPointStationChange: PropTypes.func.isRequired,
+  onDestinationStationChange: PropTypes.func.isRequired,
 };
 
-/** Pre-condition: options.length > 0 */
 function DockingStationSelector({ label, value, stations, onChange, style }) {
-  const labelId = useId();
-
-  const handleChange = (e) => {
-    let stationId = e.target.value;
-    onChange(stationId);
-  };
-
   return (
     <div style={style}>
-      <label style={{ display: "block" }} id={labelId}>
-        {label}
-      </label>
+      <label style={{ display: "block" }}>{label}</label>
       <select
         value={value}
-        onChange={handleChange}
-        // Prevent DockedEbikeRouteOption 'onClick' when clicking on this select
+        onChange={(e) => onChange(e.target.value)}
         onClick={(e) => e.stopPropagation()}
-        aria-labelledby={labelId}
       >
         {stations.map((station) => (
           <option value={station.id} key={station.id}>
@@ -244,8 +168,8 @@ DockingStationSelector.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-    }),
-  ),
-  onChange: PropTypes.func.isRequired, // (dockingStationId) => void
+    })
+  ).isRequired,
+  onChange: PropTypes.func.isRequired,
   style: Style,
 };
